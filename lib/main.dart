157c857f74f988362bd677acade7f7d9d950f271
jdeguzman-teligent <jdeguzman@teligent.com.ph>
2021-06-05 17:40:20 +0800

    stateful now

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,62 @@
+import 'package:appcenter/appcenter.dart';
+import 'package:appcenter_analytics/appcenter_analytics.dart';
+import 'package:appcenter_crashes/appcenter_crashes.dart';
+
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 import './random_words.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform;
+import 'package:flutter/foundation.dart' show TargetPlatform;
 
+void main() => runApp(MyMainApp());
 
+class MyMainApp extends StatefulWidget {
+  @override
+  MyApp createState() => MyApp();
+}
 
-void main() => runApp(MyApp());
+class MyApp extends State<MyMainApp> {
+  String _appSecret = 'Unknown';
+  String _installId = 'Unknown';
+  bool _areAnalyticsEnabled = false, _areCrashesEnabled = false;
 
-class MyApp extends StatelessWidget {
+  MyApp() {
+    final ios = defaultTargetPlatform == TargetPlatform.iOS;
+    _appSecret = ios
+        ? "40f17c96-1911-4b2b-8933-6a3c61200156"
+        : "737fc290-7e3e-4099-9625-e1e812dd2442";
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  initPlatformState() async {
+    await AppCenter.start(
+        _appSecret, [AppCenterAnalytics.id, AppCenterCrashes.id]);
+
+    await AppCenter.setEnabled(true); // global
+    await AppCenterAnalytics.setEnabled(true); // just a service
+    await AppCenterCrashes.setEnabled(true); // just a service
+
+    print('initState is called.');
+    if (!mounted) return;
+
+    var installId = await AppCenter.installId;
+
+    var areAnalyticsEnabled = await AppCenterAnalytics.isEnabled;
+    var areCrashesEnabled = await AppCenterCrashes.isEnabled;
+
+    setState(() {
+      _installId = installId;
+      _areAnalyticsEnabled = areAnalyticsEnabled;
+      _areCrashesEnabled = areCrashesEnabled;
+    });
+  }
+
+  @override
+  initState() {
+    super.initState();
+    initPlatformState();
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -15,3 +66,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
+
+// final ios = defaultTargetPlatform == TargetPlatform.iOS;
+// var app_secret = ios ? "40f17c96-1911-4b2b-8933-6a3c61200156" : "737fc290-7e3e-4099-9625-e1e812dd2442";
+
+// await AppCenter.start(app_secret, [AppCenterAnalytics.id, AppCenterCrashes.id]);
+
+// await AppCenter.setEnabled(false); // global
+// await AppCenterAnalytics.setEnabled(false); // just a service
+// await AppCenterCrashes.setEnabled(false); // just a service
